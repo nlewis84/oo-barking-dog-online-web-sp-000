de9ebd54dc6cc02c72cb07fb4231706035cebdf0
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-26 20:18:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,14 @@
 # Your code goes here!
 class Dog
-  def name=(this_dogs_name)
-    
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
 end
